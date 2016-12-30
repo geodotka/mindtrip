228b7354a49ab2fe801e667c7c5168a08be66d6e
geodotka <dorota.kowalik@21s.pl>
2016-12-31 00:00:47 +0100

    Add contact page.

--- mindtrip/trips/views.py
+++ mindtrip/trips/views.py
@@ -7,3 +7,8 @@ from annoying.decorators import render_to
 @render_to('trips/base.html')
 def home(request):
     return {}
+
+
+@render_to('trips/contact.html')
+def contact(request):
+    return {}
