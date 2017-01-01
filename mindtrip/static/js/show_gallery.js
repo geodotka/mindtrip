dd43825294fc62284a623f3cf819459b086e2cd3
geodotka <dorota.kowalik@21s.pl>
2017-01-01 22:53:29 +0100

    Show gallery with js.

---
+++ mindtrip/static/js/show_gallery.js
@@ -0,0 +1,54 @@
+
+var Gallery = function(tripsJson){
+    this.trips = this.getTrips(tripsJson);
+    console.log(this.trips);
+    this.firstTripIndex = 0;
+};
+
+
+Gallery.prototype.getTrips = function(tripsJson){
+    var trips = JSON.parse(tripsJson);
+    var tripList = [];
+    for (var i= 0; i < trips.length; i++) {
+        tripList.push(new Trip(trips[i]))
+    }
+    return tripList
+};
+
+
+Gallery.prototype.draw = function(){
+    var $gallery = $('.js-gallery');
+    $gallery.empty();
+    $gallery.append(this.trips[this.firstTripIndex].drawTripWidget());
+    $gallery.append(this.trips[this.firstTripIndex + 1].drawTripWidget())
+};
+
+
+var Trip = function(trip){
+    this.id = trip.pk;
+    this.destination = trip.fields.destination;
+    this.pictureUrl = '/media/' + trip.fields.picture;
+    this.startAt = trip.fields.start_at;
+    this.endAt = trip.fields.end_at;
+};
+
+
+Trip.prototype.drawTripWidget = function(){
+    var tripContainer = document.createElement('div');
+
+    var img = document.createElement('img');
+    img.src = this.pictureUrl;
+    tripContainer.appendChild(img);
+
+    var destination = document.createElement('span');
+    destination.innerHTML = this.destination;
+    destination.className = 'destination';
+    tripContainer.appendChild(destination);
+
+    var dates = document.createElement('span');
+    dates.innerHTML = this.startAt + ' - ' + this.endAt;
+    dates.className = 'dates';
+    tripContainer.appendChild(dates);
+
+    return tripContainer
+};
--- mindtrip/trips/views.py
+++ mindtrip/trips/views.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import json
+
 from annoying.decorators import render_to
 from django.core import serializers
 
@@ -10,8 +12,8 @@ from .models import Trip
 @render_to('trips/home.html')
 def home(request):
     trips = serializers.serialize('json', Trip.objects.all().order_by(
-        'start_at'), fields=('destination', 'picture', 'start_at', 'end_at'))
-    return {'trips': trips}
+        '-start_at'), fields=('destination', 'picture', 'start_at', 'end_at'))
+    return {'trips': json.dumps(trips)}
 
 
 @render_to('trips/contact.html')
