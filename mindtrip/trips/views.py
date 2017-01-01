c4d95f74f562b053e97835ede8f5393930315af0
geodotka <dorota.kowalik@21s.pl>
2017-01-01 21:04:33 +0100

    Distinguish base template. Update contact page.

--- mindtrip/trips/views.py
+++ mindtrip/trips/views.py
@@ -2,11 +2,16 @@
 # encoding: utf-8
 
 from annoying.decorators import render_to
+from django.core import serializers
+
+from .models import Trip
 
 
-@render_to('trips/base.html')
+@render_to('trips/home.html')
 def home(request):
-    return {}
+    trips = serializers.serialize('json', Trip.objects.all().order_by(
+        'start_at'), fields=('destination', 'picture', 'start_at', 'end_at'))
+    return {'trips': trips}
 
 
 @render_to('trips/contact.html')
