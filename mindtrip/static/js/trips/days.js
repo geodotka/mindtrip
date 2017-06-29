d894996bfadc5cc7f9f4c9c62f0cf00553eb7b59
geodotka <dorota.kowalik@21s.pl>
2017-06-29 23:08:18 +0200

    #6: style trip page

--- mindtrip/static/js/trips/days.js
+++ mindtrip/static/js/trips/days.js
@@ -1,12 +1,13 @@
 (function () {
-    $('.js-show-more').on('click', function(){
-        var $boxDescription = $(this).parent().find('.js-day-box-description');
-        if ($boxDescription.css('display') == 'none') {
-            $boxDescription.show();
-            $('.js-show-more-info').html('Zwiń<i class="material-icons">keyboard_arrow_up</i>');
+    $('.js-show-more').on('click', function() {
+        var $this = $(this);
+        var $dayDescription = $this.parent().find('.js-day-description');
+        if ($dayDescription.css('display') == 'none') {
+            $dayDescription.show();
+            $this.find('.js-show-more-info').html('Zwiń<i class="material-icons">keyboard_arrow_up</i>');
         } else {
-            $boxDescription.hide();
-            $('.js-show-more-info').html('Rozwiń<i class="material-icons">keyboard_arrow_down</i>');
+            $dayDescription.hide();
+            $this.find('.js-show-more-info').html('Rozwiń<i class="material-icons">keyboard_arrow_down</i>');
         }
     })
 })();
