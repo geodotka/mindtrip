5193de3840bd2054e80a9c56a197bc4de737a8f9
geodotka <dorota.kowalik@21s.pl>
2017-08-20 09:49:46 +0200

    Show post created_at date in admin panel

--- mindtrip/trips/admin.py
+++ mindtrip/trips/admin.py
@@ -6,10 +6,14 @@ from django.contrib import admin
 from .models import Trip, Day, Tag, Country, Photo, News, Post
 
 
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', )
+
+
 admin.site.register(Trip)
 admin.site.register(Day)
 admin.site.register(Tag)
 admin.site.register(Country)
 admin.site.register(Photo)
 admin.site.register(News)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
