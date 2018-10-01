88788c3c4deb4d66e399769255e869250df114ed
geodotka <dorota.kowalik@21s.pl>
2018-10-01 19:26:46 +0200

    Add google search key

--- mindtrip/urls.py
+++ mindtrip/urls.py
@@ -5,9 +5,13 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     url(r'^geodotka/', admin.site.urls),
+    url(r'^google7daa94b016bce91b.html',
+        TemplateView.as_view(template_name='google.html')),
 
     url(r'^', include('mindtrip.quizzes.urls')),
     url(r'^', include('mindtrip.trips.urls')),
