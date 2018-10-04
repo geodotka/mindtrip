70f71c30b4a930e03385104962134f45733717c5
geodotka <dorota.kowalik@21s.pl>
2018-10-04 16:14:25 +0200

    Add gwara quizz

--- mindtrip/quizzes/urls.py
+++ mindtrip/quizzes/urls.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from django.conf.urls import url
+from django.views.generic.base import TemplateView
 
 from . import views
 
@@ -9,4 +10,6 @@ from . import views
 urlpatterns = [
     url(r'^quizy$', views.get_quizzes, name='quizzes'),
     url(r'^quizy/(?P<quiz_id>\d+)$', views.get_quiz, name='quiz'),
+    url(r'^quizy/gwara$',
+        TemplateView.as_view(template_name='quizzes/gwara.html')),
 ]
