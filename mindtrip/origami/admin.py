90b6965481b8a68a02f60da26fb2958ffeaa7a63
geodotka <dorota.kowalik@21s.pl>
2018-11-24 23:29:20 +0100

    Add Item model in origami app

--- mindtrip/origami/admin.py
+++ mindtrip/origami/admin.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
+from django.contrib import admin
+
+from .models import Item
+
+admin.site.register(Item)
--- mindtrip/origami/models.py
+++ mindtrip/origami/models.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
+import os
+
+from django.db import models
+
+
+def photo_upload_to(instance, filename):
+    return os.sep.join(
+        ['origami', str(instance.id), filename])
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255, verbose_name=u'Nazwa')
+    description = models.TextField(null=True, blank=True, verbose_name=u'Opis')
+    photo1 = models.ImageField(
+        upload_to=photo_upload_to, verbose_name=u'Zdjęcie 1')
+    photo2 = models.ImageField(
+        upload_to=photo_upload_to, null=True, blank=True,
+        verbose_name=u'Zdjęcie 2')
+    diagram_url = models.URLField(
+        null=True, blank=True, verbose_name=u'Adres diagramu')
+    video_url = models.URLField(
+        null=True, blank=True, verbose_name=u'Adres video')
+
+    class Meta:
+        verbose_name = u'Origami'
+        verbose_name_plural = u'Origami'
+
+    def __unicode__(self):
+        return self.name
