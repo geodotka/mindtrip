c3958b08235c02180a965c7f6d6cb023769912c5
geodotka <dorota.kowalik@21s.pl>
2020-01-06 18:26:09 +0100

    Create new layout with materialize

--- mindtrip/static/js/trips/days.js
+++ mindtrip/static/js/trips/days.js
@@ -1,13 +1,69 @@
 (function () {
-    $('.js-show-more').on('click', function() {
-        var $this = $(this);
-        var $dayDescription = $this.parent().find('.js-day-description');
-        if ($dayDescription.css('display') == 'none') {
-            $dayDescription.show();
-            $this.find('.js-show-more-info').html('Zwiń<i class="material-icons">keyboard_arrow_up</i>');
+
+    const uncolorButtons = ($button, $parent) => {
+        $parent.querySelectorAll('a').forEach(($b) => {
+            $b.classList.remove('btn-active');
+        });
+    };
+
+    const showSection = ($section, $button) => {
+        if ($section.classList.contains('hidden')) {
+            $section.classList.remove('hidden');
+            $button.classList.add('btn-active');
         } else {
-            $dayDescription.hide();
-            $this.find('.js-show-more-info').html('Rozwiń<i class="material-icons">keyboard_arrow_down</i>');
+            $section.classList.add('hidden');
+            $button.classList.remove('btn-active');
         }
-    })
+    };
+
+    document.querySelectorAll('.js-description').forEach(($button) => {
+        $button.addEventListener('click', (ev) => {
+            const $parent = $button.parentNode.parentNode.parentNode;
+            uncolorButtons($button, $parent);
+            showSection($parent.querySelector('.js-day-description'), $button);
+
+            const $photos = $parent.querySelector('.js-day-photos');
+            if ($photos) {
+                $photos.classList.add('hidden');
+            }
+            const $tips = $parent.querySelector('.js-day-tips');
+            if ($tips) {
+                $tips.classList.add('hidden');
+            }
+        });
+    });
+
+    document.querySelectorAll('.js-photos').forEach(($button) => {
+        $button.addEventListener('click', (ev) => {
+            const $parent = $button.parentNode.parentNode.parentNode;
+            uncolorButtons($button, $parent);
+            showSection($parent.querySelector('.js-day-photos'), $button);
+
+            const $description = $parent.querySelector('.js-day-description');
+            if ($description) {
+                $description.classList.add('hidden');
+            }
+            const $tips = $parent.querySelector('.js-day-tips');
+            if ($tips) {
+                $tips.classList.add('hidden');
+            }
+        });
+    });
+
+    document.querySelectorAll('.js-tips').forEach(($button) => {
+        $button.addEventListener('click', (ev) => {
+            const $parent = $button.parentNode.parentNode.parentNode;
+            uncolorButtons($button, $parent);
+            showSection($parent.querySelector('.js-day-tips'), $button);
+
+            const $description = $parent.querySelector('.js-day-description');
+            if ($description) {
+                $description.classList.add('hidden');
+            }
+            const $photos = $parent.querySelector('.js-day-photos');
+            if ($photos) {
+                $photos.classList.add('hidden');
+            }
+        });
+    });
 })();
